b53ebdd81d5830647bba3131f9cd5f91823df5c0
Linh kieu hoa <mr.linh1090@gmail.com>
2021-07-10 14:17:59 +0700

    Section 2: Node - Project Setup

---
+++ routes/user.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+
+const { sayHi } = require('../controllers/userController');
+
+router.get('/', sayHi);
+
+
+
+
+
+module.exports = router;
